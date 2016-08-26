03b5511529f4369b118ba7b3d24a5a9db48dcc36
chris-breed <christopher.breed@my.jcu.edu.au>
2016-08-26 17:56:10 +1000

    Deck and Card start

---
+++ src/BuildDeck.java
@@ -0,0 +1,32 @@
+public class BuildDeck {
+    public static void main(String[] args){
+
+        getDeck();
+
+
+    }
+
+
+    private static String[][] getDeck(){
+        String[] deck = new String[54];
+
+
+        for (int i = 0; i <= 54; i++){
+            deck[i] = getCard();
+        }
+
+        return deck;
+    }
+
+    private static String[] getCard(){
+        String[] cards;
+
+
+
+
+
+
+
+        return cards;
+    }
+}
