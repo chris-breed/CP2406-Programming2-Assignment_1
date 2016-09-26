1067aac9398e39fa8c3e94f1dc1907f67f74f510
chris-breed <christopher.breed@my.jcu.edu.au>
2016-09-26 15:46:20 +1000

    SuperTrumpGame - user selection of the number of players and checking that value is within the right boundries

---
+++ src/SuperTrumpGame.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class SuperTrumpGame {
+
+    public int playerCount;
+
+    public static void main(String[] args) {
+
+        /*if start -
+        ask user how many players there are (1 user player, between 3 and 4 bots)
+        start game
+        randomly select dealer
+        randomly distribute the cards*/
+        boolean running = false;
+
+        initializeGame();
+
+
+    }
+
+    private static void initializeGame(){
+        checkPlayerCount();
+        selectDealer();
+
+    }
+
+    private static int selectDealer() {
+        int dealerPosition = 0;
+
+
+        return dealerPosition;
+    }
+
+
+    private static int checkPlayerCount() {
+        boolean playersCountBool = false;
+        int playerCount;
+
+        playerCount = getPlayerCount();
+
+        while (!playersCountBool) {
+            if (!(playerCount <= 5 && playerCount >= 3)) {
+                System.out.println("\n\n\n\n\n\nEnter a number of players between 3 and 5 (including the player)");
+                playerCount = getPlayerCount();
+            } else {
+                playersCountBool = true;
+
+
+                //array for the players
+                //array for the cards - then reference the deck array
+            }
+        }
+
+        return playerCount;
+    }
+
+    private static int getPlayerCount() {
+        int noPlayers;
+        Scanner menuSelection = new Scanner(System.in);
+        System.out.println("How many players are there? (Between 3 and 5)");
+        noPlayers = menuSelection.nextInt();
+
+        return noPlayers;
+    }
+}
